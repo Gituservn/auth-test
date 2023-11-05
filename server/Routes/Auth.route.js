bdbfad0be36593effc6457d46518d21f5ed2885d
Evgeniy <zhrkacomua@gmail.com>
2023-11-05 22:46:19 +0200

    commit

---
+++ server/Routes/Auth.route.js
@@ -0,0 +1,12 @@
+import {Signup,login}  from "../Controllers/Auth.controller.js";
+import express from "express";
+import userVerification from "../Middlewares/Auth.middleware.js";
+
+const router = express.Router();
+
+ router.post("/signup", Signup);
+ router.post("/login", login);
+ router.post("/",userVerification)
+
+
+export default router;
